19b75915d80c624abeaa70fae48dd10efeb2857d
kwiato88 <kwiato88@outlook.com>
2017-10-16 13:43:12 +0800

    cleanup includes in CTagsMT.cpp

--- nppCtagPlugin/Tests/CTagsMT.cpp
+++ nppCtagPlugin/Tests/CTagsMT.cpp
@@ -1,34 +1,4 @@
-#include <gtest/gtest.h>
-#include <memory>
-#include <boost/algorithm/string/replace.hpp>
-#include <exception>
-#include <typeinfo>
-#include <sstream>
-#include <memory>
-#include <algorithm>
-#include "TestsGlobals.hpp"
 #include "CTagsMT.hpp"
-#include "MatcherHasMoreThanOneElem.hpp"
-#include "MatcherEndsWith.hpp"
-#include "MatcherTags.hpp"
-
-#include "CppTag.hpp"
-#include "CppIsTagWithAtt.hpp"
-#include "CTagsNavigator.hpp"
-#include "Navigator.hpp"
-#include "TagFileReader.hpp"
-#include "CachedTagsReader.hpp"
-#include "TagNotFoundException.hpp"
-#include "TagsReaderException.hpp"
-#include "OpenFileException.hpp"
-#include "LocationGetterMock.hpp"
-#include "LocationSetterMock.hpp"
-#include "TagsSelectorMock.hpp"
-#include "TagHierarchySelectorMock.hpp"
-#include "TagParser.hpp"
-
-#include "Logger.hpp"
-#include "Log.hpp"
 
 namespace CTagsPlugin
 {
