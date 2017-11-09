9caca1227f2e0a1976f882c94c424f791410fb05
kwiato88 <kwiato88@outlook.com>
2017-11-09 14:09:27 +0800

    removed openfile from runt tests scripts

--- runTests.ps1
+++ runTests.ps1
@@ -1,5 +1,5 @@
 dir .\Build\nppCtagPlugin\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
 dir .\Build\nppHeaderSourceSwitcher\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
 dir .\Build\nppIncludeBrowser\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
-dir .\Build\nppOpenFile\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
+#dir .\Build\nppOpenFile\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
 dir .\Build\nppCtagPlugin\Release\*MT.exe | ForEach-Object { & $_.FullName }
--- runUt.ps1
+++ runUt.ps1
@@ -1,4 +1,4 @@
 dir .\Build\nppCtagPlugin\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
 dir .\Build\nppHeaderSourceSwitcher\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
 dir .\Build\nppIncludeBrowser\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
-dir .\Build\nppOpenFile\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
+#dir .\Build\nppOpenFile\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
