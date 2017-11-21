efbf0297ff54d7288abf28e5c9f5dfe4fb0a201a
kwiato88 <kwiato88@outlook.com>
2017-11-21 13:25:03 +0800

    deleted log workaround done for ctags MT

--- Utils/Include/Log.hpp
+++ Utils/Include/Log.hpp
@@ -1,13 +1,5 @@
 #pragma once
 
-//#define CONSOLE_LOG
-
 #include <string>
 #include "Logger.hpp"
-
-//TODO: workaround for linker problems in MT. Can't link boost logger
-#if defined(CONSOLE_LOG)
-#include "LogConsole.hpp"
-#else
 #include "LogBoost.hpp"
-#endif
