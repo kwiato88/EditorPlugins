cc643f043f420fb8ed5bf215ee180753b6b87c83
kwiato88 <kwiato88@outlook.com>
2018-04-27 13:43:42 +0800

    nppOpenFile: added cliet lib

---
+++ nppOpenFile/Include/ExternalInterface/OpenFileExternalCommand.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "ExternalCommand.hpp"
+
+namespace NppPlugin
+{
+
+class OpenFileExternalCommand
+{
+public:
+	OpenFileExternalCommand(HWND p_npp, const std::string& p_source, const std::string& p_target = "nppOpenFile.dll");
+
+	/**
+	* @throws NppPlugin::ExternalCommandFailure
+	*/
+	void setSearchDirs(const std::vector<std::string>& p_dirPaths);
+	/**
+	* @throws NppPlugin::ExternalCommandFailure
+	*/
+	std::vector<std::string> getSearchDirs();
+	/**
+	* @throws NppPlugin::ExternalCommandFailure
+	*/
+	std::vector<std::string> findFiles(const std::vector<std::string>& p_serachDirs,
+		const std::string& p_fileNamePatter, bool p_caseSesitive);
+
+private:
+    NppPlugin::ExternalCommand cmd;
+};
+
+}
---
+++ nppOpenFile/Source/OpenFileExternalCommand.cpp
@@ -0,0 +1,44 @@
+#include "OpenFileExternalCommand.hpp"
+#include "Commands.hpp"
+#include "Results.hpp"
+
+namespace NppPlugin
+{
+
+OpenFileExternalCommand::OpenFileExternalCommand(
+    HWND p_npp,
+    const std::string& p_source, const std::string& p_target)
+    : cmd(p_npp, p_target, p_source)
+{}
+
+void OpenFileExternalCommand::setSearchDirs(const std::vector<std::string>& p_dirPaths)
+{
+	OpenFileCommand::SetSearchDirs command;
+	command.dirsPaths = p_dirPaths;
+	auto result = cmd.invokeWithResult<OpenFileCommand::SetSearchDirs, OpenFileResult::Basic>(command);
+	if (result.res == OpenFileResult::Result::Failure)
+		throw NppPlugin::ExternalCommandFailure("Failed to set search dirs. Received failure from plugin");
+}
+
+std::vector<std::string> OpenFileExternalCommand::getSearchDirs()
+{
+	OpenFileCommand::GetSearchDirs command;
+	auto result = cmd.invokeWithResult<OpenFileCommand::GetSearchDirs, OpenFileResult::SearchDirs>(command);
+	return result.paths;
+}
+
+std::vector<std::string> OpenFileExternalCommand::findFiles(
+	const std::vector<std::string>& p_serachDirs,
+	const std::string& p_fileNamePatter, bool p_caseSesitive)
+{
+	OpenFileCommand::FindFiles command;
+	command.dirsPaths = p_serachDirs;
+	command.fileNamePattern = p_fileNamePatter;
+	command.caseSensitiveSearch = p_caseSesitive;
+	auto result = cmd.invokeWithResult<OpenFileCommand::FindFiles, OpenFileResult::Files>(command);
+	if (result.res == OpenFileResult::Result::Failure)
+		throw NppPlugin::ExternalCommandFailure("Failed to find files. Received failure from plugin");
+	return result.paths;
+}
+
+}
