1266acf59161c94577bdf250ecf25b00b2c16b09
kwiato88 <kwiato88@outlook.com>
2019-04-06 11:12:56 +0200

    nppCtagsPlugin: fix MT

--- runUt.ps1
+++ runUt.ps1
@@ -1,7 +1,7 @@
 #$env:projectRootPath = "D:\test\EditorPlugins\"
 $env:projectRootPath = "D:\Projects\EditorPlugins\"
 #$env:projectRootPathWith2Slash = "D:\\test\\EditorPlugins\\"
-$env:projectRootPath = "D:\\Projects\\EditorPlugins\\"
+$env:projectRootPathWith2Slash = "D:\\Projects\\EditorPlugins\\"
 dir .\Build\nppCtagPlugin\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
 dir .\Build\nppHeaderSourceSwitcher\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
 dir .\Build\nppIncludeBrowser\Debug\*Tests.exe | ForEach-Object { & $_.FullName }
