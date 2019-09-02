2f0a8656b0a546b88a76743e3e655616b33f22f4
kwiato88 <kwiato88@outlook.com>
2019-09-02 21:26:15 +0200

    nppCtagsPlugin: more class diagram tests

--- nppCtagPlugin/Tests/ClassDiagramTS.cpp
+++ nppCtagPlugin/Tests/ClassDiagramTS.cpp
@@ -64,4 +64,56 @@ TEST_F(ClassDiagramTS, classWithOneBaseShoulHaveRelation)
 	ASSERT_EQ(beginMarker + "base <|-- derived\n" + endMarker, buff.str());
 }
 
+TEST_F(ClassDiagramTS, classWith2BaseShoulHave2Relations)
+{
+	{
+		ClassDiagram::Class derived{ TestTag{"derived"} };
+		derived.addBase(TestTag{ "base" });
+		derived.addBase(TestTag{ "base1" });
+
+		ClassDiagram diagram{ buff };
+		diagram.add(derived);
+	}
+	ASSERT_EQ(beginMarker + "base <|-- derived\n" + "base1 <|-- derived\n" + endMarker, buff.str());
+}
+
+TEST_F(ClassDiagramTS, 2classWithOneBaseShoulHave2Relations)
+{
+	{
+		ClassDiagram::Class derived1{ TestTag{"derived1"} };
+		derived1.addBase(TestTag{ "base1" });
+
+		ClassDiagram diagram{ buff };
+		diagram.add(derived1);
+
+		ClassDiagram::Class derived2{ TestTag{"derived2"} };
+		derived2.addBase(TestTag{ "base1" });
+
+		diagram.add(derived2);
+	}
+	ASSERT_EQ(beginMarker + "base1 <|-- derived1\n" + "base1 <|-- derived2\n" + endMarker, buff.str());
+}
+
+TEST_F(ClassDiagramTS, 2classWith2BaseShoulHave4Relations)
+{
+	{
+		ClassDiagram::Class derived1{ TestTag{"derived1"} };
+		derived1.addBase(TestTag{ "base1" });
+		derived1.addBase(TestTag{ "base2" });
+
+		ClassDiagram diagram{ buff };
+		diagram.add(derived1);
+
+		ClassDiagram::Class derived2{ TestTag{"derived2"} };
+		derived2.addBase(TestTag{ "base1" });
+		derived2.addBase(TestTag{ "base3" });
+
+		diagram.add(derived2);
+	}
+	ASSERT_EQ(beginMarker
+		+ "base1 <|-- derived1\n" + "base2 <|-- derived1\n"
+		+ "base1 <|-- derived2\n" + "base3 <|-- derived2\n"
+		+ endMarker, buff.str());
+}
+
 }
