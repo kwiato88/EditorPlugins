b47f98c9bf39139f7d500fd2a7a1b69d493357cb
kwiato88 <kwiato88@outlook.com>
2019-09-03 22:23:30 +0200

    nppCtagsPlugin: more MTs for class diagram

--- nppCtagPlugin/Tests/ClassDiagramMT.cpp
+++ nppCtagPlugin/Tests/ClassDiagramMT.cpp
@@ -1,7 +1,9 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-
+#include <boost/algorithm/string/split.hpp>
+#include <boost/algorithm/string/classification.hpp>
 #include <sstream>
+#include <algorithm>
 
 #include "ComplexTagWithMatchingName.hpp"
 #include "CTagsMT.hpp"
@@ -9,6 +11,33 @@
 namespace CTagsPlugin
 {
 using namespace	::testing;
+
+MATCHER_P(HasRelations, expectedRelations, "")
+{
+	std::vector<std::string> lines;
+	boost::split(lines, arg, boost::is_any_of("\n"));
+
+	if (expectedRelations.size() != lines.size() - 3)
+	{
+		*result_listener << "\nMismatch number of expected relations\n"
+			<< "  Expected: " << expectedRelations.size() << "\n"
+			<< "  Actual:   " << (lines.size() - 3) << "\n";
+		return false;
+	}
+	for (const auto& relation : expectedRelations)
+		if (std::find(lines.begin(), lines.end(), relation) == lines.end())
+		{
+			*result_listener << "\nDidn't find expected relation: " << relation << "\n";
+			*result_listener << "  Expected relations:\n";
+			for (const auto& r : expectedRelations)
+				*result_listener << r << "\n";
+			*result_listener << "  Actual relations:\n";
+			for (unsigned int i = 1; i < (lines.size() - 2); ++i)
+				*result_listener << lines.at(i) << "\n";
+			return false;
+		}
+	return true;
+}
 
 struct ClassDiagramMT : public CTagsMT
 {
@@ -18,12 +47,24 @@ struct ClassDiagramMT : public CTagsMT
 	std::ostringstream diagram;
 };
 
-TEST_F(ClassDiagramMT, generateDiagramForCppTag)
+TEST_F(ClassDiagramMT, generateDiagramForSingleTag)
 {
 	tagsNavigator.exportClassDiagram(diagram, ComplexTagWithMatchingName("CTagsPlugin::CppTag"));
 	ASSERT_EQ(
 		diagramBegin + "CTagsPlugin::Tag <|-- CTagsPlugin::CppTag\n" + diagramEnd,
 		diagram.str());
+}
+
+TEST_F(ClassDiagramMT, generateDiagramForMultipleTags)
+{
+	tagsNavigator.exportClassDiagram(diagram, ComplexTagWithMatchingName(".*Reader"));
+	EXPECT_THAT(
+		diagram.str(), 
+		HasRelations(std::vector<std::string>({
+			"CTagsPlugin::ITagsReader <|-- CTagsPlugin::TagFileReader",
+			"CTagsPlugin::ITagsReader <|-- CTagsPlugin::MultipleTagFilesReader",
+			"CTagsPlugin::ITagsReader <|-- CTagsPlugin::FileScopedTagFilteringReader"
+		})));
 }
 
 }
